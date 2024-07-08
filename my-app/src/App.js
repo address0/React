1ee595d7340f9ff48700d6b4b464024956d8f19f
address0 <soyong6624@gmail.com>
2024-07-08 19:37:36 +0900

    4일차: JSX 작성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,10 +8,22 @@ function App() {
       <img
       src='https://i.imgur.com/MK3eW3Am.jpg'
       alt="Katherine Johnson"
+      class="photo"
       />
       <hr />
       <Hello />
       <Goodbye />
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+      src="https://i.imgur.com/yXOvdOSs.jpg"
+      alt="Hedy Lamarr"
+      class="photo"
+      />
+      <ul>
+          <li>Invent <b>new traffic lights</b></li>
+          <li>Rehearse a movie scene</li>
+          <li>Improve the <i>spectrum technology</i></li>
+      </ul>
     </div>
   );
 }
