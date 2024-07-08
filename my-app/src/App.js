8f33d0d0774d18b37530ceb8c6608ea34a69c73c
address0 <soyong6624@gmail.com>
2024-07-08 20:16:35 +0900

    4일차: JSX 동적 속성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,16 @@ import Hello from './Hello';
 import { Goodbye } from './Hello';
 
 function App() {
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg'
+  const description = 'Hedy Lamarr'
+  const today = new Date();
+  const person = {
+    name: 'SSAFY',
+    theme: {
+      backgroundColor: 'black',
+      color: 'pink'
+    }
+  }
   return (
     <div>
       <img
@@ -24,8 +34,22 @@ function App() {
           <li>Rehearse a movie scene</li>
           <li>Improve the <i>spectrum technology</i></li>
       </ul>
+      <h1 style={{color: 'blue'}}>{description}'s photo on {formatDate(today)}</h1>
+      <img
+      className="avatar"
+      src={avatar}
+      alt={description}
+      />
+      <h3 style={person.theme}>Hi, my name is {person.name}!</h3>
     </div>
   );
+}
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    {weekday:'long'}
+  ).format(date);
 }
 
 export default App;
